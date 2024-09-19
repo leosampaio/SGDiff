5e52cfaa4ad1e88df6c13e13878587777f3a99c9
Leo Sampaio Ferraz Ribeiro <leo.sampaio.ferraz.ribeiro@gmail.com>
2024-09-19 10:04:44 -0300

    Add demo notebook and fix dependencies/links

--- scripts/download_vg.sh
+++ scripts/download_vg.sh
@@ -1,12 +1,12 @@
 VG_DIR=datasets/vg
 mkdir -p $VG_DIR
 
-wget https://visualgenome.org/static/data/dataset/objects.json.zip -O $VG_DIR/objects.json.zip
-wget https://visualgenome.org/static/data/dataset/attributes.json.zip -O $VG_DIR/attributes.json.zip
-wget https://visualgenome.org/static/data/dataset/relationships.json.zip -O $VG_DIR/relationships.json.zip
-wget https://visualgenome.org/static/data/dataset/object_alias.txt -O $VG_DIR/object_alias.txt
-wget https://visualgenome.org/static/data/dataset/relationship_alias.txt -O $VG_DIR/relationship_alias.txt
-wget https://visualgenome.org/static/data/dataset/image_data.json.zip -O $VG_DIR/image_data.json.zip
+wget https://homes.cs.washington.edu/~ranjay/visualgenome/data/dataset/objects.json.zip -O $VG_DIR/objects.json.zip
+wget https://homes.cs.washington.edu/~ranjay/visualgenome/data/dataset/attributes.json.zip -O $VG_DIR/attributes.json.zip
+wget https://homes.cs.washington.edu/~ranjay/visualgenome/data/dataset/relationships.json.zip -O $VG_DIR/relationships.json.zip
+wget https://homes.cs.washington.edu/~ranjay/visualgenome/data/dataset/object_alias.txt -O $VG_DIR/object_alias.txt
+wget https://homes.cs.washington.edu/~ranjay/visualgenome/data/dataset/relationship_alias.txt -O $VG_DIR/relationship_alias.txt
+wget https://homes.cs.washington.edu/~ranjay/visualgenome/data/dataset/image_data.json.zip -O $VG_DIR/image_data.json.zip
 wget https://cs.stanford.edu/people/rak248/VG_100K_2/images.zip -O $VG_DIR/images.zip
 wget https://cs.stanford.edu/people/rak248/VG_100K_2/images2.zip -O $VG_DIR/images2.zip
 
